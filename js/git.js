aec13cb8754b86b5839edb24a70b903b00822193
Estherrika <esther95l8l75@gmail.com>
2024-02-15 16:05:15 +0200

    Set Ball Growth Limit

--- js/git.js
+++ js/git.js
@@ -3,11 +3,20 @@
 function onBallClick(elBall) {
     var ballSize = elBall.innerText
     var newBAllSize = `${+ballSize + 50}px`
+    console.log(newBAllSize);
+    if (elBall.style.width === '400px') {
+        newBAllSize = "100px"
+        ballSize = 100
+        elBall.innerText = 100
 
+
+    } else {
+        elBall.innerText = (+ballSize + 50)
+    }
     elBall.style.width = newBAllSize
     elBall.style.height = newBAllSize
-    
-    elBall.innerText = (+ballSize + 50)
+
+
 
 
 }
