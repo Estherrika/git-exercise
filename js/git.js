bd790d30f85cc83099c346c321d6d8054ce07831
Estherrika <esther95l8l75@gmail.com>
2024-02-15 16:46:07 +0200

    Change Ball Color on Click

--- js/git.js
+++ js/git.js
@@ -2,21 +2,22 @@
 
 function onBallClick(elBall) {
     var ballSize = elBall.innerText
-    var RandomSize=getRandomInt(20,60)
-    var newBAllSize = `${+ballSize+RandomSize}px`
+    var RandomSize = getRandomInt(20, 60)
+    var newBAllSize = `${+ballSize + RandomSize}px`
+
     if (+ballSize >= 400) {
         newBAllSize = "100px"
         ballSize = 100
         elBall.innerText = 100
-
-
     } else {
         elBall.innerText = (+ballSize + RandomSize)
     }
+
+    //increase ball size onclick
     elBall.style.width = newBAllSize
     elBall.style.height = newBAllSize
 
-
-
+    //change ball color on click
+    elBall.style.backgroundColor = getRandomColor()
 
 }
--- js/util.js
+++ js/util.js
@@ -1,5 +1,19 @@
+'use script'
+
 function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-  }
+
+}
+
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
